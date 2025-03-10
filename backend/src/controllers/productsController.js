27330de2eb4f9c6e78484afc2ceffaebd949c7bd
Messi <20230253@ricaldone.edu.sv>
2025-03-10 09:29:47 -0600

    ......

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -7,9 +7,29 @@ import productsModel from "../models/Products.js";
 productsController.getProducts = async (req, res) => {
     const products = await productsModel.find()
     res.json(products)
-}
+};
 
 // INSERT
-productsController.postProducts = async (req, res) => {
-    
-}
+productsController.insertProducts = async (req, res) => {
+    const { name, description, price, stock } = req.body;
+    const newProduct = new productsModel({name, description, price, stock});
+    await newProduct.save()
+    res.json({message: "product saved"});
+};
+
+// DELETE
+productsController.deleteProduct = async (req, res) => {
+    await productsModel.findByIdAndDelete(req.params.id)
+    res.json({message: "product delete"});
+};
+
+// UPDATE
+productsController.updateProduct = async (req, res) => {
+    const { name, description, price, stock } = req.body;
+    const updateProduct = await productsModel.findByIdAndUpdate(
+        req.params.id, {name, description, price, stock}, {new: true}
+    )
+    res.json({message: "product updated"});
+};
+
+export default productsController;
